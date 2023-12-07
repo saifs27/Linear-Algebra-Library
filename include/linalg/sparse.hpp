349b000f105defcbd7be590e6250f17e7d2d12b0
Saif <113651806+saifs27@users.noreply.github.com>
2023-12-07 00:36:02 -0600

    updated sparse

---
+++ include/linalg/sparse.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vector>
+#include <complex>
+
+template <typename T> 
+class SparseMatrix {
+    private:
+    int n_rows;
+    int n_columns;
+    std::vector<T> data; //excludes zeros
+    std::vector<int> column_index;
+    std::vector<int> row_index;
+
+    public:
+    SparseMatrix();
+};
---
+++ src/sparse.cpp
@@ -0,0 +1,8 @@
+#include "../include/linalg/sparse.hpp"
+
+template <typename T>
+SparseMatrix<T>::SparseMatrix() {
+    n_columns = 0;
+    n_rows = 0;
+    
+}
